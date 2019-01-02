40f942c6125c5fcf112727eba987bc0e6240650c
Peter Faso <quinlayen@gmail.com>
2019-01-02 10:52:47 -1000

    changed directory to include getArtList to replace handler

---
+++ getArtList.js
@@ -0,0 +1,50 @@
+"use strict";
+const request = require("request-promise-native");
+const cheerio = require("cheerio");
+//const {chosenArt} = require('./helpers/getDimensions')
+
+module.exports.getArtList = async (event, context) => {
+  var query_word = "hawaiian";
+  // if (process.argv.length > 2) {
+  //   query_word = process.argv[2];
+  // }
+
+  // Send query to the Chicago museum site
+  let url = "https://www.artic.edu/collection?q=" + query_word;
+
+ 
+
+  let baseReq = await request(url);
+    
+    try {
+    
+      const $ = cheerio.load(baseReq);
+     //console.log('from dimensions', chosenArt())
+      var links = [];
+      $("#artworksList")
+        .children()
+        .each(function() {
+          let $link = $(this)
+            .find("a")
+            .attr("href");
+          links.push($link);
+        });
+
+      if (links.length > 0) {
+        console.log(
+          "Found " + links.length + " '" + query_word + "' pictures.\n"
+        );
+        links.forEach(function(item) {
+          console.log(item);
+        });
+      } else {
+        console.log("No image found for '" + query_word + "'");
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+
+
+  
